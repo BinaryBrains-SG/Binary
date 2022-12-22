237003cd2dd61778c14d72e32e62c9a794b3ff69
gowtham4545 <gowthamgopi444@gmail.com>
2022-12-22 19:04:46 +0530

    constants

--- binary/settings.py
+++ binary/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8ary4&t_d9rc!n)jd5f8)iif+ksx^vfm)t^j_+kvn9&(j4yp1q'
+from .CONSTANTS import *
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
